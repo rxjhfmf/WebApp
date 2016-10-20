356d0980e907fbb8abfffdff4a7c73c120b9d3eb
Frank <396465370@qq.com>
2016-10-20 21:44:31 +0800

    修复BUG

--- tool/weather.py
+++ tool/weather.py
@@ -5,6 +5,7 @@ import json
 from urllib import request
 from urllib import parse
 
+
 # ======================================================
 # 通过百度天气API，获取天气信息
 # API请求参数：
@@ -18,7 +19,7 @@ class Weather(object):
         self.__key = '1fb0f26807b883c97ba10d3ce7ad8c38'
         self.__city = city
 
-    @staticmethod          # 静态方法
+    @staticmethod  # 静态方法
     def handle_request(url):
         req = request.Request(url)
         with request.urlopen(req) as response:
@@ -28,30 +29,33 @@ class Weather(object):
     def get_weather(self, city=''):
         if city is not None:
             self.__city = city
-        url = '%s?location=%s&output=json&ak=%s' % (self.__url_weather, parse.quote(self.__city), self.__key)
+        url = '%s?location=%s&output=json&ak=%s' % (
+            self.__url_weather, parse.quote(self.__city), self.__key)
         print("%s" % url)
         #return
         self.__weather = self.handle_request(url)
 
         data_json = json.loads(self.__weather)
-        if data_json['error']!=0:  
-            print ('检查城市输入是否有误')
-            return 
-        print ("%s:%s" % (data_json['results'][0]['currentCity'],data_json['date']))  
+        if data_json['error'] != 0:
+            print('检查城市输入是否有误')
+            return
+        print("%s:%s" %
+              (data_json['results'][0]['currentCity'], data_json['date']))
 
-        for t in data_json['results'][0]['weather_data']:  
-            print (t['date'])
-            print (t['temperature'])  
-            print (t['weather'])  
-            print (t['wind'])  
-            print ("")
+        for t in data_json['results'][0]['weather_data']:
+            print(t['date'])
+            print(t['temperature'])
+            print(t['weather'])
+            print(t['wind'])
+            print("")
 
         for t in data_json['results'][0]['index']:
-            print(t['title']+':'+t['zs'])
-            print(t['tipt']+':'+t['des'])
+            print(t['tipt'] + ':' + t['zs'])
+            print(t['title'] + ':' + t['des'])
             print('')
         return self.__weather
 
+
 if __name__ == '__main__':
     weather = Weather()
-    weather.get_weather('杭州')
+    weather.get_weather('宁波')
--- www/app/routes.py
+++ www/app/routes.py
@@ -7,15 +7,14 @@ __author__ = 'Frank Fu'
 import re, time, json, logging, hashlib, base64, asyncio
 from app.frame import get, post
 from .models import  *
+from urllib import parse
 from app.frame.helper import Page, set_valid_value
-
-def ch2utf(name):
-    a=name.encode('utf-8',"s")
-    return "".join(str(a).split("\\x"))[2:-1]
+from markdown import markdown
 
 @get('/blog/{id}')
 async def get_blog(id):
     blog = await Blog.find(id)
+    blog.content=markdown(blog.content,['codehilite']);
     sid="blog"+str(blog.id)
     tags=await Tag.findAll(orderBy="name")
     return {
@@ -34,10 +33,9 @@ def index():
 @get('/photos')
 async def get_photos(request):
 
-    str="a.jpg"
     photos=[
-        Photo(name="name1",alt="alt1",title='title1',src=ch2utf("me.jpg")),
-        Photo(name="name2",alt="alt2",title='title2',src=ch2utf("A_头像.jpg")),
+        Photo(name="name1",alt="alt1",title='title1',src=parse.quote("me.jpg")),
+        Photo(name="name2",alt="alt2",title='title2',src=parse.quote("A.jpg")),
     ]
 
     return{
@@ -47,8 +45,6 @@ async def get_photos(request):
 
 @get('/blogs')
 async def get_blogs(request, *, tag='Python', page='1', size='5'):
-    if not tag.endswith('"'):
-        tag="\""+tag+"\""
     num = await Blog.countRows(where="position(? in `tag`)", args=[tag])
     page = Page(num, set_valid_value(page), set_valid_value(size, 10))
     tags=await Tag.findAll(orderBy="name")
@@ -56,6 +52,9 @@ async def get_blogs(request, *, tag='Python', page='1', size='5'):
         blogs = []
     else:
         blogs = await Blog.findAll("position(? in `tag`)", [tag], orderBy='created_at desc', limit=(page.offset, page.limit))
+    
+    for blog in blogs:
+        blog.content=markdown(blog.content,['codehilite']);
 
     return {
         '__template__': 'blogs.html',
