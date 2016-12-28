852e10eb5cbe27da69174ce27d7cba57c744f508
rxjhfmf <396465370@qq.com>
2016-12-28 14:19:57 +0800

    修改调试配置

--- www/test.py
+++ www/test.py
@@ -1,24 +1,62 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-def f(x):
-	return "0x%x" %x
-
-def printhex(l):
-	var=""
-	for x in l:
-		var+=f(x)+' '
-	print(var)
-
-fo = open('foo.bin','rb+')
-print ("文件名: ", fo.name)
-print ("是否已关闭 : ", fo.closed)
-print ("访问模式 : ", fo.mode)
-
-a=fo.read()
-print(a)
-b=list(a)
-printhex(b)
-b.append(0x03)
-c=bytes(b'hhhh')
-fo.write(c)
+#! /usr/bin/env python3
+import gzip
+import re
+import http.cookiejar
+import urllib.request
+import urllib.parse
+ 
+def ungzip(data):
+    try:        # 尝试解压
+        print('正在解压.....')
+        data = gzip.decompress(data)
+        print('解压完毕!')
+    except:
+        print('未经压缩, 无需解压')
+    return data
+ 
+def getXSRF(data):
+    cer = re.compile('name=\"_xsrf\" value=\"(.*)\"', flags = 0)
+    strlist = cer.findall(data)
+    print(data)
+    return strlist[0]
+ 
+def getOpener(head):
+    # deal with the Cookies
+    cj = http.cookiejar.CookieJar()
+    pro = urllib.request.HTTPCookieProcessor(cj)
+    opener = urllib.request.build_opener(pro)
+    header = []
+    for key, value in head.items():
+        elem = (key, value)
+        header.append(elem)
+    opener.addheaders = header
+    return opener
+ 
+header = {
+    'Connection': 'Keep-Alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
+    'Host': 'https://www.zhihu.com/#signin',
+}
+ 
+url = 'https://www.zhihu.com/#signin'
+opener = getOpener(header)
+op = opener.open(url)
+data = op.read()
+data = ungzip(data)     # 解压
+_xsrf = getXSRF(data.decode())
+ 
+url += '#signin'
+id = '18758870687'
+password = '123456'
+postDict = {
+        '_xsrf':_xsrf,
+        'email': id,
+        'password': password,
+        'rememberme': 'y'
+}
+postData = urllib.parse.urlencode(postDict).encode()
+op = opener.open(url, postData)
+data = op.read()
+data = ungzip(data)
+ 
+print(data.decode())
